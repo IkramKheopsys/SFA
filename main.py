53a10a77d18b904b513294de4e21407382d6d276
IkramKheopsys <113558455+IkramKheopsys@users.noreply.github.com>
2024-04-26 17:37:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from model.test_models.functions import TextGeneration,get_embeddings
+from model.test_models.functions import text_generation,get_embeddings
 
 
 def main():
 
     file_path = 'training_data.json'
 
-    model_output = TextGeneration(file_path)
+    model_output = text_generation(file_path)
 
     print("\n\n")
     print("Text Generation:")
